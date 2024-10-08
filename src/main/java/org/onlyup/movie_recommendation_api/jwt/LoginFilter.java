32a7e45164b2beb54f3797ea4a0bc977fb79e93d
hubgood98 <heej1998@gmail.com>
2024-10-08 23:59:51 +0900

    jwt 경로 token 한줄 임시추가

--- src/main/java/org/onlyup/movie_recommendation_api/jwt/LoginFilter.java
+++ src/main/java/org/onlyup/movie_recommendation_api/jwt/LoginFilter.java
@@ -1,4 +1,32 @@
 package org.onlyup.movie_recommendation_api.jwt;
 
-public class LoginFilter {
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+
+    private final AuthenticationManager authenticationManager;
+
+    public LoginFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException{
+
+        //클라이언트 요청에서 username,password 추출
+        String accountId = obtainUsername(request);
+        String password = obtainPassword(request);
+
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(accountId, password,null);
+
+        //Token에 담은 검증을 위한 AuthenticationManager
+        return authenticationManager.authenticate(authToken);
+    }
 }
