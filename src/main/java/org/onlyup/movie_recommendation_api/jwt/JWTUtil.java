0d94eae9e5eda4f3b388853d150376b6f6521b46
hubgood98 <heej1998@gmail.com>
2024-10-11 01:49:16 +0900

    JWTUtil 클래스 작성

---
+++ src/main/java/org/onlyup/movie_recommendation_api/jwt/JWTUtil.java
@@ -0,0 +1,54 @@
+package org.onlyup.movie_recommendation_api.jwt;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JWTUtil {
+    private SecretKey key;
+
+    JWTUtil(@Value("${spring.jwt.secret}") String secret){
+        this.key = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8), Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
+
+    public String getAccountId(String token) {
+
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload().get("accountId", String.class);
+    }
+
+    public String getRole(String token) {
+
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload().get("role", String.class);
+    }
+
+    public Boolean isExpired(String token) {
+
+        return Jwts.parser().verifyWith(key).build().parseSignedClaims(token).getPayload().getExpiration().before(new Date());
+    }
+
+    public String createJwt(String accouontId, String role, Long expiredMs) {
+
+        return Jwts.builder()
+                .claim("accountId", accouontId)
+                .claim("role", role)
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + expiredMs))
+                .signWith(key)
+                .compact();
+    }
+
+}
