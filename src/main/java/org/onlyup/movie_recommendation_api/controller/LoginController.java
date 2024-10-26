cc7647828c5011341bea1aeb1424891f3f8d9a3e
hubgood98 <heej1998@gmail.com>
2024-10-26 19:17:31 +0900

    애너테이션 controller -> restcontroller로 변경

--- src/main/java/org/onlyup/movie_recommendation_api/controller/LoginController.java
+++ src/main/java/org/onlyup/movie_recommendation_api/controller/LoginController.java
@@ -1,10 +1,10 @@
 package org.onlyup.movie_recommendation_api.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/user")
 public class LoginController {
 
--- src/main/java/org/onlyup/movie_recommendation_api/controller/RegisterController.java
+++ src/main/java/org/onlyup/movie_recommendation_api/controller/RegisterController.java
@@ -2,11 +2,11 @@ package org.onlyup.movie_recommendation_api.controller;
 
 import org.onlyup.movie_recommendation_api.dto.user.RegisterRequest;
 import org.onlyup.movie_recommendation_api.service.user.UserRegisterService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @ResponseBody
 public class RegisterController {
 
