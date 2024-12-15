e783d6cced19602a34a15f3a86ee6e24d1fceeaf
hubgood98 <heej1998@gmail.com>
2024-12-16 02:48:24 +0900

    시큐리티6버전에 맞게 메소드 수정

--- src/main/java/org/onlyup/movie_recommendation_api/config/SecurityConfig.java
+++ src/main/java/org/onlyup/movie_recommendation_api/config/SecurityConfig.java
@@ -50,7 +50,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-                .cors(Customizer.withDefaults())
+                .cors(c->c.configurationSource(corsConfigurationSource()))
                 //csrf
                 .csrf(AbstractHttpConfigurer::disable)
                 //from 로그인방식
