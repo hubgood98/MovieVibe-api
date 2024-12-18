31d72f75ce250dd58fa995eee46eceb4046a2f65
hubgood98 <heej1998@gmail.com>
2024-12-19 02:26:52 +0900

    평가 생성 및 갱신될 때 점수반영 문제 해결

--- src/main/java/org/onlyup/movie_recommendation_api/repository/RatingRepository.java
+++ src/main/java/org/onlyup/movie_recommendation_api/repository/RatingRepository.java
@@ -1,11 +1,9 @@
 package org.onlyup.movie_recommendation_api.repository;
 
-import feign.Param;
 import org.onlyup.movie_recommendation_api.domain.Movie;
 import org.onlyup.movie_recommendation_api.domain.Rating;
 import org.onlyup.movie_recommendation_api.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
